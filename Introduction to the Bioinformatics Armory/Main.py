7ee4c63b62e69a439baca038d3be1c2602383260
Saumitra Kapoor <saumitrakapoor21@gmail.com>
2017-01-02 01:16:42 +0530

    Create Main.py

---
+++ Introduction to the Bioinformatics Armory/Main.py
@@ -0,0 +1,16 @@
+adenine = 0 // Variable for storing A count
+guanine = 0 // Variable for storing G count
+cytosine = 0 // Variable for storing C count
+thymine = 0 // Variable for storing T count
+ss = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
+for word in ss:
+  if w == 'A':
+    aa = aa + 1
+  if w == 'T':
+    tt = tt + 1
+  if w == 'G':
+    gg = gg + 1
+  if w == 'C':
+    cc = cc + 1
+print str(aa) + " " + str(cc) + " " + str(gg) + " " + str(tt)
+
