c8c388637615710dd03adec2fe2dfa808a4ed404
Saumitra Kapoor <saumitrakapoor21@gmail.com>
2017-01-02 13:43:53 +0530

    Create Main.py

---
+++ Complementing a Strand of DNA/Main.py
@@ -0,0 +1,15 @@
+s = 'AAAACCCGGT' // The input Sequence
+ss = s[::-1] // Reverse the String
+w = []
+for ww in ss:
+  if ww == 'A':
+    w.append('T')
+  if ww == 'T':
+    w.append('A')
+  if ww == 'G':
+    w.append('C')
+  if ww == 'C':
+    w.append('G')
+n = len(w)
+for p in range(n):
+  print w[p],
