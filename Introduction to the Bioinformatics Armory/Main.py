acf5fb0e0c89ff83ea8b303cbb2577684d8cf6b4
Saumitra Kapoor <saumitrakapoor21@gmail.com>
2017-01-04 21:38:27 +0530

    Update Main.py

--- Introduction to the Bioinformatics Armory/Main.py
+++ Introduction to the Bioinformatics Armory/Main.py
@@ -1,8 +1,8 @@
-adenine = 0 // Variable for storing A count
-guanine = 0 // Variable for storing G count
-cytosine = 0 // Variable for storing C count
-thymine = 0 // Variable for storing T count
-ss = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC' // A given sequence
+adenine = 0 # Variable for storing A count
+guanine = 0 # Variable for storing G count
+cytosine = 0 # Variable for storing C count
+thymine = 0 # Variable for storing T count
+ss = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC' # A given sequence
 for word in ss:
   if w == 'A':
     adenine = adenine + 1
